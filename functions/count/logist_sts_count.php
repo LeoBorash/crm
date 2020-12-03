40c68fcd398aca0735ac180bfa9920b1486dc396
Leo <maulenborashov@gmail.com>
2020-12-03 19:55:24 +0600

    Count Бага, logist_sts_count.php

--- functions/count/logist_sts_count.php
+++ functions/count/logist_sts_count.php
@@ -4,29 +4,29 @@ function logist_sts_count()
     global $conn;
     if ($_SESSION['user_status']==2 or $_SESSION['user_status']==4) { ?>
         <?php
-        if($_SESSION['user_name']=='Бағлан'){
+        if($_SESSION['user_name']=='Бағла'){
 
-            $query = $conn->query("SELECT count(client_price) as total3 FROM clients WHERE client_status = '2,1.Алматы' AND client_confirm = 'Бағлан'");
+            $query = $conn->query("SELECT count(client_price) as total3 FROM clients WHERE client_status = '2,1.Алматы' AND client_confirm = 'Бағла'");
             $row = mysqli_fetch_assoc($query); ?>
             <th class="text-center main-status">Алматы (<?= $row['total3'] ?>)</th>
             <?php
-            $query = $conn->query("SELECT count(client_price) as total2 FROM clients WHERE client_status = '2.Подтвержден' AND client_confirm = 'Бағлан'");
+            $query = $conn->query("SELECT count(client_price) as total2 FROM clients WHERE client_status = '2.Подтвержден' AND client_confirm = 'Бағла'");
             $row = mysqli_fetch_assoc($query); ?>
             <th class="text-center main-status">Подтвержден (<?= $row['total2'] ?>)</th>
             <?php
-            $query = $conn->query("SELECT count(client_price) as total6 FROM clients WHERE client_status = '4,1.Ожидает доставки' AND client_confirm = 'Бағлан'");
+            $query = $conn->query("SELECT count(client_price) as total6 FROM clients WHERE client_status = '4,1.Ожидает доставки' AND client_confirm = 'Бағла'");
             $row = mysqli_fetch_assoc($query); ?>
             <th class="text-center main-status">Ожидает доставки (<?= $row['total6'] ?>)</th>
             <?php
-            $query = $conn->query("SELECT count(client_price) as total8 FROM clients WHERE client_status = '5.В Пути' AND client_confirm = 'Бағлан'");
+            $query = $conn->query("SELECT count(client_price) as total8 FROM clients WHERE client_status = '5.В Пути' AND client_confirm = 'Бағла'");
             $row = mysqli_fetch_assoc($query); ?>
             <th class="text-center main-status">В Пути (<?= $row['total8'] ?>)</th>
             <?php
-            $query = $conn->query("SELECT count(client_price) as total9 FROM clients WHERE client_status = '4,3.Поступил' AND client_confirm = 'Бағлан'");
+            $query = $conn->query("SELECT count(client_price) as total9 FROM clients WHERE client_status = '4,3.Поступил' AND client_confirm = 'Бағла'");
             $row = mysqli_fetch_assoc($query); ?>
             <th class="text-center main-status">Поступил (<?= $row['total9'] ?>)</th>
             <?php
-            $query = $conn->query("SELECT count(client_price) as total11 FROM clients WHERE client_status in('7.Отказ','8.Отказ') AND client_confirm = 'Бағлан'");
+            $query = $conn->query("SELECT count(client_price) as total11 FROM clients WHERE client_status in('7.Отказ','8.Отказ') AND client_confirm = 'Бағла'");
             $row = mysqli_fetch_assoc($query); ?>
             <th class="text-center main-status">Отказ (<?= $row['total11'] ?>)</th>
 
