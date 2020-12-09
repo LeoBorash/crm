6bd6beaf8fa863f8bc51df60812f4670ade765e3
Leo <maulenborashov@gmail.com>
2020-12-09 17:23:11 +0600

    СДЭК Статус добавление

--- includes/db.php
+++ includes/db.php
@@ -1,7 +1,7 @@
 <?php
 // $conn = mysqli_connect("srv-pleskdb29.ps.kz", "itdockz1_crm", "Nl3&e1b0", "itdockz1_crm");
-$conn = mysqli_connect("localhost", "root", "", "asyl_bd");
+//$conn = mysqli_connect("localhost", "root", "", "asyl_bd");
 //$conn = mysqli_connect("srv-pleskdb29.ps.kz", "itdockz1_baga", "Gmoj57@0", "itdockz1_baga");
 //$conn = mysqli_connect("srv-pleskdb29.ps.kz", "itdockz1_aika", "Ogen3_49", "itdockz1_aika");
 //$conn = mysqli_connect("185.146.3.237", "leoborash", "MyPassword2", "leoborash");
-//$conn = mysqli_connect("srv-pleskdb29.ps.kz", "itdockz1_m", "w3m?6v1P", "itdockz1_m");
+$conn = mysqli_connect("srv-pleskdb29.ps.kz", "itdockz1_m", "w3m?6v1P", "itdockz1_m");
